650093d6e71a2a55c697589b33d51cdb5d52d2ab
Manu <manuthecoder@protonmail.com>
2022-04-29 08:15:56 -0700

    Change cringe headline XD

--- pages/index.js
+++ pages/index.js
@@ -14,59 +14,59 @@ import Fab from "@mui/material/Fab";
 export const AccentColor = "#dc2626";
 
 export default function IndexPage() {
-	return (
-		<div>
-			<Box
-				sx={{
-					display: "flex",
-					alignItems: "center",
-					height: "100vh",
-					textAlign: "center",
-					width: "100vw",
-					justifyContent: "center",
-					color: "white",
-					background: "url(https://i.ibb.co/2gYpC2Z/image.png)",
-					backgroundSize: "cover",
-					backgroundRepeat: "no-repeat"
-				}}
-			>
-				<Box sx={{ px: 3 }}>
-					<Toolbar />
-					<Typography className="font-mono">
-						Hey there! My name is Manu...{" "}
-					</Typography>
-					<Typography
-						variant="h1"
-						sx={{
-							fontWeight: "bold",
-							fontSize: { xs: "60px", sm: "100px" }
-						}}
-					>
-						and I develop apps that help everyone
-					</Typography>
-				</Box>
-				<Fab
-					onClick={() => window.scrollTo(0, 500)}
-					sx={{
-						background: "#000!important",
-						color: "#fff",
-						position: "absolute",
-						boxShadow: 0,
-						bottom: 15,
-						left: "50%",
-						transform: "translateX(-50%)"
-					}}
-				>
-					<Grow in={true}>
-						<ExpandMoreIcon />
-					</Grow>
-				</Fab>
-			</Box>
+  return (
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          height: "100vh",
+          textAlign: "center",
+          width: "100vw",
+          justifyContent: "center",
+          color: "white",
+          background: "url(https://i.ibb.co/2gYpC2Z/image.png)",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat"
+        }}
+      >
+        <Box sx={{ px: 3 }}>
+          <Toolbar />
+          <Typography className="font-mono">
+            Hey there! My name is Manu...{" "}
+          </Typography>
+          <Typography
+            variant="h1"
+            sx={{
+              fontWeight: "bold",
+              fontSize: { xs: "60px", sm: "100px" }
+            }}
+          >
+            and I develop apps, websites &amp; discord bots
+          </Typography>
+        </Box>
+        <Fab
+          onClick={() => window.scrollTo(0, 500)}
+          sx={{
+            background: "#000!important",
+            color: "#fff",
+            position: "absolute",
+            boxShadow: 0,
+            bottom: 15,
+            left: "50%",
+            transform: "translateX(-50%)"
+          }}
+        >
+          <Grow in={true}>
+            <ExpandMoreIcon />
+          </Grow>
+        </Fab>
+      </Box>
 
-			<WhatDoIDo />
-			<Specializations />
-			<ProjectList />
-			<Awards />
-		</div>
-	);
+      <WhatDoIDo />
+      <Specializations />
+      <ProjectList />
+      <Awards />
+    </div>
+  );
 }
